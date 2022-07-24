120e5dd8893f595c2a4095cfb1e05f23b65493d9
Ryan <thisianemailaddressthatis@gmail.com>
2022-07-23 19:00:58 -0500

    added search bar and button and some logic

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var cityFind = document.getElementById("city-find");
+
+$(".btn").on("click", function (event) {
+  event.preventDefault();
+  //   console.log(event.target.id);
+  //   for (let i = 0; i < hours.length; i++) {
+  //     if (hours[i].btn === event.target.id) {
+  //       const value = document.getElementById(hours[i].el).value;
+  //       hours[i].text = value;
+  //     }
+  //   }
+  console.log("beef");
+  //   https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+});
